b7a2e25c8d7d55524707f7bffad40941fda66511
Leonardo Jesus <leonardojesus02@outlook.com>
2020-11-09 00:19:29 -0300

    Add State and Immutability concepts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState(['App Development', 'Front-end web']);
+
+  function handleAddProject() {
+    setProjects([...projects, `New project ${Date.now()}`]);
+    
+    console.log(projects);
+  };
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
+      <Header title="Projects"/>
+
       <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Add Project
+      </button>
     </>
   )
 };
